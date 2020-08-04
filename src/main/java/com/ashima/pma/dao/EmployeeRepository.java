4d8d9412e211dbb3d1526ceef440e82ff1f9da7f
ashima <ashima.sood15@gmail.com>
2020-08-04 10:01:46 +0200

    Update and Delete
    Added update and delete functionalities to project and employee sections

--- src/main/java/com/ashima/pma/dao/EmployeeRepository.java
+++ src/main/java/com/ashima/pma/dao/EmployeeRepository.java
@@ -2,11 +2,8 @@ package com.ashima.pma.dao;
 
 import java.util.List;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ashima.pma.dto.EmployeeProject;
